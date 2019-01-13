885f3ebdf54fa582c3bf0ee4e300f86512cd257d
luolaifa000 <18510023526@163.com>
2019-01-13 23:16:24 +0800

    service层也按照约定规则自动映射生成对应的model

--- App/Service/UserService.php
+++ App/Service/UserService.php
@@ -5,17 +5,10 @@ use App\Model\UserModel;
 
 class UserService extends Service
 {
-    protected $userModel;
-    
-    public function __construct()
-    {
-        $this->userModel = new UserModel();
 
-    }
-    
-    
     public function getUserInfo()
     {
+        
         $data = $this->userModel->getUserInfo();
         #逻辑处理
         return $data;
--- Framework/Library/Service/Service.php
+++ Framework/Library/Service/Service.php
@@ -1,5 +1,7 @@
 <?php
 namespace Twinkle\Library\Service;
+
+use Twinkle\Library\Service\ServiceLocatorTrait;
 /**
  * 基础SERVICE层,控制层来调用这层
  * 这层必须调MODEL层---请遵守规则
@@ -9,6 +11,17 @@ namespace Twinkle\Library\Service;
  * */
 class Service
 {
+    
+    use ServiceLocatorTrait;
+    
+    public static function supportAutoNamespaces() 
+	{
+        return [
+            'App\\Model'
+        ];
+    }
+    
+    
     public function __construct()
     {
 
